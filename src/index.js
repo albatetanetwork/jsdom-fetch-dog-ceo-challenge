ef1751d7b7cdde3805d08d7bdb5865edc95af051
Alba <albatetanetwork@gmail.com>
2021-04-11 18:50:16 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 console.log('%c HI', 'color: firebrick')
+function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+      .then(response=> response.json())
+      .then(results => {
+        results.message.forEach(image => addDogImage(image))
+      });
+  }
+
+  function addDogImage(dogImgUrl) {
+    const container = document.querySelector('#dog-image-container');
+     newImageEl = document.createElement('img');
+    newImageEl.src = dogImgUrl;
+    container.appendChild(newImageEl);
+  }
+  const breeds = [];
+
+  document.addEventListener('DOMContentLoaded', function () {
+    loadImages();
+    
+  });
+
+  const breedUrl = "https://dog.ceo/api/breeds/list/all"
+ fetch(breedUrl)
+ .then(res=> res.json())
+ .then(results => {
+
+    breeds = Object.keys(results.message);
+    updateBreedList(breeds);
+    addBreedSelectListener();
+ })
+
+ function updateBreedList(breeds) {
+    let ul = document.querySelector('#dog-breeds');
+    removeChildren(ul);
+    breeds.forEach(breed => addBreed(breed));
+  }
+  function selectBreedsStartingWith(letter) {
+    updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+  }
+  
